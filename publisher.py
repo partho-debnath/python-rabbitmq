9fbccb4b039ddb53f513ac5a5751dc5e2ca0b0fe
Partho Debnath <parthodebnath28@gmail.com>
2024-09-26 00:32:40 +0600

    publisher updated

--- publisher.py
+++ publisher.py
@@ -1,3 +1,5 @@
+import json
+import time
 from rabbit_connection import get_rabbitmq_connection
 
 
@@ -8,10 +10,16 @@ def publish_message():
         channel = connection.channel()
         queue_name = "hello_queue"
         channel.queue_declare(queue=queue_name)
-        message = "hello universe - 1"
-        channel.basic_publish(exchange="", routing_key=queue_name, body=message)
+        for i in range(1, 100000):
+            message = {f"key_{i}": f"value {i}"}
+            channel.basic_publish(
+                exchange="",
+                routing_key=queue_name,
+                body=json.dumps(message),
+            )
+            print(f"Published '{message}' message.")
+            time.sleep(1)
         channel.close()
-        print(f"Published '{message}' message.")
     except Exception as e:
         print("Error", e)
 
